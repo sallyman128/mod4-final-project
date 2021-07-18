1b70be13df1a247a821fb5b3e5d1b5b6acabf9c5
Salmaan <salmaan.arif.ali@gmail.com>
2021-07-18 17:50:50 -0400

    build migrations

--- backend/db/migrate/20210718214303_create_items.rb
+++ backend/db/migrate/20210718214303_create_items.rb
@@ -1,7 +1,8 @@
 class CreateItems < ActiveRecord::Migration[6.1]
   def change
     create_table :items do |t|
-
+      t.string :name
+      t.float :price
       t.timestamps
     end
   end
--- backend/db/migrate/20210718214311_create_carts.rb
+++ backend/db/migrate/20210718214311_create_carts.rb
@@ -1,7 +1,7 @@
 class CreateCarts < ActiveRecord::Migration[6.1]
   def change
     create_table :carts do |t|
-
+      t.string :name
       t.timestamps
     end
   end
