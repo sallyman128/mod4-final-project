b2092c4b585c85a702c7a97752b96f9589198449
Salmaan <salmaan.arif.ali@gmail.com>
2021-07-20 15:11:05 -0400

    build skeleton of route actions

--- backend/app/controllers/notes_controller.rb
+++ backend/app/controllers/notes_controller.rb
@@ -1,2 +1,20 @@
 class NotesController < ApplicationController
+
+  def index
+
+  end
+
+  def create
+
+  end
+
+  def delete
+
+  end
+
+  private
+  # def note_params(params)
+  #   params.require(note).permit()
+  # end
+
 end
