7ca8e54d4871493da0424c2237118f8a7cbecbd0
Salmaan <salmaan.arif.ali@gmail.com>
2021-07-20 15:13:04 -0400

    remove unnecessary autogenerated reoutes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :tags
-  resources :notes
+  # resources :tags
+  resource :notes, only: [:index, :create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
