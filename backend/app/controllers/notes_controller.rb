2624c15d3752e473522a51d52e08b25c8f3cc771
Salmaan <salmaan.arif.ali@gmail.com>
2021-07-20 15:58:15 -0400

    render index jsoon

--- backend/app/controllers/notes_controller.rb
+++ backend/app/controllers/notes_controller.rb
@@ -1,7 +1,8 @@
 class NotesController < ApplicationController
 
   def index
-
+    @notes = Note.all
+    render :json => @notes
   end
 
   def create
--- backend/app/controllers/tags_controller.rb
+++ backend/app/controllers/tags_controller.rb
@@ -1,2 +1,21 @@
 class TagsController < ApplicationController
+
+  def index
+    @tags = Tag.all
+    render :json => @tags
+  end
+
+  def create
+
+  end
+
+  def destroy
+
+  end
+
+  private
+  
+  # def tag_params
+
+  # end
 end
