10105fada0a1ceb94a33bc75218e3837c82a9b0f
Salmaan <salmaan.arif.ali@gmail.com>
2021-07-20 15:07:52 -0400

    add to body to each seed note

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -6,9 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
 5.times do |i|
-  note = Note.create(title: "##{i+1} Note")
+  note = Note.create(title: "##{i+1} Note", body: "A filler body to the note.")
   note.tags << Tag.create(name: "First tag")
   note.tags << Tag.create(name: "Second tag")
 end
