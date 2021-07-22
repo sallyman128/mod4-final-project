8bdac98e1603472743120e85d21855c9ded7d3f3
Salmaan <salmaan.arif.ali@gmail.com>
2021-07-22 12:58:29 -0400

    fetch all Notes from rails api

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,11 +8,12 @@ class Note {
 
   static all() {
     return fetch(`http://localhost:3000/notes`)
-    .then( resp => resp.json() )
+      .then( resp => resp.json() )
+      .then( result => result )
   }
 }
 
-// add the appropirate HTML meta tags that will allow link to js source code and eventual css
+// DONE add the appropirate HTML meta tags that will allow link to js source code and eventual css
 // first get a js class working that can retrieve data from the rails api
 // then once that it working, display that info to the DOM
 // then build form to add to that list
