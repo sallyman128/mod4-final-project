6c23663ac7e0996393121352a87697006e460b7a
Salmaan <salmaan.arif.ali@gmail.com>
2021-07-22 14:36:51 -0400

    trying to append notes to notesContainer dibiv

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,5 @@
-const baseUrl = 'http://localhost/3000';
+const baseUrl = 'http://localhost:3000';
+const notesContainerDiv = document.getElementById("notesContainer");
 
 const getAllNotes = () => {
   return fetch(`${baseUrl}/notes`)
@@ -8,9 +9,18 @@ const getAllNotes = () => {
 
 const renderNotes = (notes) => {
   notes.forEach( (note) => {
-    const {title, body} = note
+    const {title, body} = note;
+    const template = `
+      <p>
+        <h2>Title: ${title}</h2>
+        <li>${body}</li>
+      </p>
+    `;
+    console.log(notesContainerDiv);
   })
 }
+
+document.addEventListener("DOMContentLoaded", getAllNotes)
 
 // DONE add the appropirate HTML meta tags that will allow link to js source code and eventual css
 // first get a js class working that can retrieve data from the rails api
