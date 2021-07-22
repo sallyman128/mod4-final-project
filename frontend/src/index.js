68b5224c865a3498d8fb5cfbe1e21ee66ee03ef0
Salmaan <salmaan.arif.ali@gmail.com>
2021-07-21 22:10:23 -0400

    link jsto html and build Note class

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,19 @@
 class Note {
-  baseUrl = 'http://localhost/3000';
+  // const baseUrl = 'http://localhost/3000';
 
-  get all() {
-    fetch(`${baseUrl}/notes`)
-    .then( resp => resp.json())
-    .then( json => console.log(json))
+  constructor(title, body) {
+    this.title = title;
+    this.body = body;
+  }
+
+  static all() {
+    return fetch(`http://localhost:3000/notes`)
+    .then( resp => resp.json() )
   }
 }
+
+// add the appropirate HTML meta tags that will allow link to js source code and eventual css
+// first get a js class working that can retrieve data from the rails api
+// then once that it working, display that info to the DOM
+// then build form to add to that list
+// then build delete functionality
