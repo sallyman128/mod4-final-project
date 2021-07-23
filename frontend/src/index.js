f3fd77ebbcc98763c1bd1bafa6c9102961cec951
Salmaan <salmaan.arif.ali@gmail.com>
2021-07-22 23:05:23 -0400

    finish displaying Notes and associated Tags to DOM

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,5 @@
+/*********************Append all Notes + Tags to DOM***************** */
+
 const baseUrl = 'http://localhost:3000';
 
 const getAllNotes = () => {
@@ -13,27 +15,41 @@ const renderNotes = (notes) => {
     notes.forEach( (note) => {
       const {id, title, body, tags} = note;
       let bodyTemplate = `
-        <h3>Title: ${title}</h3>
+        <h3>Title: ${title} <button id=${id}>Delete Note</button></h3>
         <div class="publishedNoteBody">
           <p>${body}</p>
-          <button id=${id}>Delete Note</button>
         </div>
       `;
-      tags.forEach( (tag) => {
-        const {tagId, tagName} = tag;
-        let tagTemplate = `
-          <li>${tagName}</li>
-          <button id=${tagId}>X</button>
-        `;
-        debugger;
-        bodyTemplate.innerHTML += tagTemplate;
-      })
+
       notesContainerDiv.innerHTML += bodyTemplate;
+
+      tags.forEach( (tag) => {
+        let tagTemplate = `
+          <p class="publishedTag" id=${tag.id}>
+            ${tag.name}
+          </p>
+        `;
+        notesContainerDiv.innerHTML += tagTemplate;
+      })
+ 
     }) 
   }
 }
 
 document.addEventListener("DOMContentLoaded", getAllNotes)
+
+/**********************Form to create a new note****************************/
+
+const postNewNote = (event) => {
+  event.preventDefault();
+}
+
+const newNoteForm = document.querySelector("#newNoteForm")
+newNoteForm.addEventListener("submit", postNewNote)
+
+// const postNewNote = (event) => {
+//   event.preventDefault();
+// }
 
 // DONE add the appropirate HTML meta tags that will allow link to js source code and eventual css
 // first get a js class working that can retrieve data from the rails api
