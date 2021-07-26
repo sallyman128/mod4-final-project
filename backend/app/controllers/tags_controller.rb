26b54a3ed581bbaa75b59f10187b49407db52cdc
Salmaan <salmaan.arif.ali@gmail.com>
2021-07-26 04:09:03 -0400

    remove unnecessary comments

--- backend/app/controllers/tags_controller.rb
+++ backend/app/controllers/tags_controller.rb
@@ -1,12 +1,8 @@
 class TagsController < ApplicationController
 
   def create
-    # tag = Tag.create(name: tag_params[:name]);
     tag = Tag.create(tag_params)
-    # note = Note.find_by_id(tag_params[:note_id])
-    # tag.notes << note
     render json: tag, only: [:id, :name, :note_id]
-    # , include: { note: {only: [:id]}}
   end
 
   def destroy
